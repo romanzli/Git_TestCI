84e9fcfa984aeec743066901563957c1db26777c
Roman <376489098@qq.com>
2017-06-01 14:33:34 +0800

    添加项目文件。

---
+++ Git_TestCI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_TestCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我们是来自Git_TestCI");
+        }
+    }
+}
