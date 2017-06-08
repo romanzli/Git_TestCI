baee292f8c574aab27ad24308dd3a9683bbcf2ed
Roman <376489098@qq.com>
2017-06-08 13:25:00 +0800

    第五次测试

--- Git_TestCI/Program.cs
+++ Git_TestCI/Program.cs
@@ -14,6 +14,7 @@ namespace Git_TestCI
             Console.WriteLine("第二次测试开始啦");
             Console.WriteLine("第三次测试开始啦");
             Console.WriteLine("第四次测试开始啦");
+            Console.WriteLine("第五次测试开始啦");
             Console.WriteLine("我们是来自Git_TestCI");
         }
     }
